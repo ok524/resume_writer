c580daf7ed82ae0ba29c435d98da490b6b5fb521
edwincheung37 <edwincheung37@gmail.com>
2020-05-02 21:33:53 +0800

    added demo feature

--- backend/resources/auth.py
+++ backend/resources/auth.py
@@ -1,10 +1,10 @@
-from flask import request
+from flask import request, current_app
 from flask_restful import Resource
-from database.documents.user import User as UserModel
+from database.documents.user import User as UserModel, Resume as ResumeModel
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 import datetime
 from mongoengine.errors import NotUniqueError, DoesNotExist, ValidationError
-
+from copy import deepcopy
 
 class User(Resource):
     @jwt_required
@@ -23,15 +23,33 @@ class Register(Resource):
     def post(self):
         try:
             user_info = request.get_json(force=True)
-            user = UserModel(**user_info)
+            if current_app.config['WEB_API_SETTING']['demo']:
+                user = deepcopy(UserModel.objects.get(email=current_app.config['WEB_API_SETTING']['demo_user']))
+                demo_user_id = user.id
+                user.id = None
+                user.email = user_info['email']
+                user.password = user_info['password']
+                user.resume = None
+            else:
+                user = UserModel(**user_info)
             user.hash_password()
             user.save()
+            if current_app.config['WEB_API_SETTING']['demo']:
+                demo_resume = deepcopy(ResumeModel.objects(created_by=demo_user_id).order_by('-created_at')[0])
+                print(demo_resume.to_json())
+                demo_resume.id = None
+                demo_resume.created_by = user.id
+                demo_resume.created_at = None
+                demo_resume.updated_at = None
+                demo_resume.save()
+
             expires = datetime.timedelta(days=7)
             access_token = create_access_token(identity=str(user.id), expires_delta=expires)
             return {'token': access_token}
         except NotUniqueError:
             return {'error': 'Email is already in use'}
-        except ValidationError:
+        except ValidationError as error:
+            print(str(error))
             return {'error': 'Invalid email address: ' + user_info['email']}
         except ValueError as error:
             return {'error': str(error)}
